99f9793823dd1ead16f01da68becb3871d9b911e
nghiapham2 <nghia.pham@vsirius.com>
2024-11-22 17:55:36 +0700

    feat

--- FillScreen/src/app/app.component.ts
+++ FillScreen/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './Components/body/body.component';
+import { FilterComponent } from './Components/filter/filter.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { LogoComponent } from "./Components/logo/logo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BodyComponent, FilterComponent, HeaderComponent, LogoComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
