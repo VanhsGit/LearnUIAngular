ae683f6a132da14d0954fd3ecdf00b06509587f2
Nghia Pham <VanhsPC@gmail.com>
2024-11-28 06:57:54 +0700

    feat

--- FillScreen/src/app/Components/filter/tipe/tipe.component.ts
+++ FillScreen/src/app/Components/filter/tipe/tipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, model, ChangeDetectionStrategy } from '@angular/core';
+import { Component, model, ChangeDetectionStrategy, ViewEncapsulation, HostBinding, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatCardModule} from '@angular/material/card';
@@ -12,10 +12,23 @@ const materialItems = [MatRadioModule, MatCardModule, MatCheckboxModule];
   imports: [FormsModule, ...materialItems],
   templateUrl: './tipe.component.html',
   styleUrl: './tipe.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush 
+  changeDetection: ChangeDetectionStrategy.OnPush ,
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'tipe-container',
+    '(click)': 'OnClickSomething()'
+  }
 })
 export class TipeComponent {
   tipes = ["Basic", "Pattern", "Hoodie", "Zipper", "Oversize"];
   readonly checked = model(false);
   readonly indeterminate = model(false);
+  @HostBinding("class") className = 'test';
+  // @HostListener('click') OnClick(){
+  //   alert("On Click host Listener");
+  // }
+
+  OnClickSomething(){
+    alert('Clicked !');
+  }
 }
