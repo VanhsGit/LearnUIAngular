1772bcd3f1f6b2862cc1397b993b3013492c885c
Nghia Pham <VanhsPC@gmail.com>
2024-12-08 21:31:33 +0700

    feat

---
+++ PurrCoffee/src/app/shared/item/side-item/side-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-item.component.html',
+  styleUrl: './side-item.component.scss'
+})
+export class SideItemComponent {
+
+}
