7bce2b72987f14827cebad78d123aef5bcfee229
nghiapham2 <nghia.pham@vsirius.com>
2024-12-15 17:51:01 +0700

    feat

--- AngularProject/src/app/app.component.spec.ts
+++ AngularProject/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'init' title`, () => {
+  it(`should have the 'AngularProject' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('init');
+    expect(app.title).toEqual('AngularProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, init');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularProject');
   });
 });
